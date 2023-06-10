27bf944b8a5f0a377d4f6e51f9cb988f584358e8
Malcolm Meyerson <malcolm_meyerson@brown.edu>
2023-06-09 20:26:00 -0400

    small changes

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # TikTokScraperFashion
 A docker runnable service that scrapes fashion videos
 
-build with
-
-docker build -t scraper_app .
-
 run with
 
 docker run -v "$(pwd)":/app my_image  
--- apiCalls.py
+++ apiCalls.py
@@ -35,7 +35,6 @@ def getExploreItemByCategory(category_type=7, count=16):
 
     
     url = base_url + "?" + "&".join([f"{key}={value}" for key, value in query_params.items()])
-    # url = "https://www.tiktok.com/api/explore/item_list/?aid=1988&app_language=en&app_name=tiktok_web&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=MacIntel&browser_version=5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F112.0.0.0%20Safari%2F537.36&categoryType=2&channel=tiktok_web&cookie_enabled=true&count=16&device_id=7239103375194932779&device_platform=web_pc&focus_state=true&from_page=&history_len=3&is_fullscreen=true&is_page_visible=true&language=en&os=mac&priority_region=&referer=&region=US&screen_height=900&screen_width=1440&tz_name=America%2FNew_York&webcast_language=en"
     response = requests.get(url)
     if response.status_code == 200:
         try:
@@ -65,6 +64,3 @@ def buildVideoObjects(response):
 
         
     return videoObjectList
-
-
-# buildVideoObjects(getExploreItemByCategory(count=16))
